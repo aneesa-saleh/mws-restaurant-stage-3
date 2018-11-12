c7451f10982521aded493bf6406bf90013b8410e
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-11-12 09:48:44 +0100

    chore: Add config files for eslint, salis and editor

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "eqeqeq": 0,
+    "no-shadow": 0,
+    "no-restricted-globals": 0,
+    "no-use-before-define": 0,
+    "no-unused-vars": 0,
+    "no-restricted-syntax": 0,
+  },
+  "env": {
+    "browser": true,
+  }
+};
